42ba59fe8e27e3207b7cc2c45fa091376db1e040
SebastianSwiczerewski <stigunknown21@gmail.com>
2026-03-11 18:06:46 +0100

    Centralize database connection config and support both Docker and local environments

--- ingestion/wait_for_db.py
+++ ingestion/wait_for_db.py
@@ -1,24 +1,23 @@
 import time
-import psycopg2
-import os
 import logging
+import psycopg2
+
+from config.database import get_db_connection
 
 LOG = logging.getLogger("wait_for_db")
+
 
 def main():
     LOG.info("Waiting for Postgres...")
 
     while True:
         try:
-            psycopg2.connect(
-                host=os.getenv("POSTGRES_HOST", "postgres"),
-                port=os.getenv("POSTGRES_PORT", "5432"),
-                user=os.getenv("POSTGRES_USER"),
-                password=os.getenv("POSTGRES_PASSWORD"),
-                dbname=os.getenv("POSTGRES_DB"),
-            )
+            conn = get_db_connection()
+            conn.close()
+
             LOG.info("Postgres is ready!")
             break
+
         except psycopg2.OperationalError:
             LOG.info("Postgres not ready yet, retrying...")
             time.sleep(2)
